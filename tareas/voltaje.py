89967914184c84a568fbda5a8818bc1a78542ab5
jasser <jassere.villarrealb@uqvirtual.edu.co>
2023-09-01 13:22:30 -0500

    subi lo hecho en clase

--- tareas/voltaje.py
+++ tareas/voltaje.py
@@ -1,1 +1,19 @@
-fijivji
+class CalculadoraVoltaje:
+    def __init__(self, intensidad, resistencia):
+        self.intensidad = intensidad
+        self.resistencia = resistencia
+
+    def calcular_voltaje(self):
+        voltaje = self.intensidad * self.resistencia
+        return voltaje
+
+# Pedir al usuario la intensidad y la resistencia
+intensidad = float(input("Introduce la intensidad en amperios: "))
+resistencia = float(input("Introduce la resistencia en ohmios: "))
+
+# Crear una instancia de la clase CalculadoraVoltaje
+calculadora = CalculadoraVoltaje(intensidad, resistencia)
+
+# Calcular y mostrar el voltaje
+voltaje_calculado = calculadora.calcular_voltaje()
+print(f"El voltaje calculado es {voltaje_calculado} voltios.")
