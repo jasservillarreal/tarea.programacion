1dcbbf5beb0c6c97bb6ac27a10a3acd6ac59019f
jasser <jassere.villarrealb@uqvirtual.edu.co>
2023-09-01 17:45:58 -0500

    practica en clase

--- tareas/practicasdeclase.py
+++ tareas/practicasdeclase.py
@@ -1,7 +1,16 @@
-x=int(input("ingrese dinero en copa depositar sin puntos ni comas: "))
-n=int(input("ingrese la cantidad de años: "))
-p=float(input("ingrese el pporcentaje de interes sin el simbolo %: "))
-for i in range(n):
-    x=x*(i+p/100)
-print("su dinero es",x,"cop")
+#x=int(input("ingrese dinero en copa depositar sin puntos ni comas: "))
+#n=int(input("ingrese la cantidad de años: "))
+#p=float(input("ingrese el pporcentaje de interes sin el simbolo %: "))
+#for i in range(n):
+#    x=x*(i+p/100)
+#print("su dinero es",x,"cop")
 
+#errores (debugging)
+#
+for i in range(10):
+    num=float(input("ingrese un numero:"))
+    if num<0:
+        print("ERROR :numero negativo:")
+        break
+else:
+    print("introdujo loS 10 valores ")
